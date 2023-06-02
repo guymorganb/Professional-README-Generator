e38e094f7b901358507361ae880e47f7d0dfd306
guymorganb <guymorganb@gmail.com>
2023-06-01 21:22:43 -0500

    got inquirer working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project',
+},
+{
+    type: 'checkbox',
+    name: 'tableOfContents',
+    message: 'Select what to include in the Table of Contents',
+    choices: [
+        'Insallation',
+        'Usage',
+        'Features',
+        'Credits',
+        'License',
+    ],
+},
+{
+    type: 'input',
+    name: 'Instalation',
+    message: 'How would you describe the instalation process?',
+},
+{
+    type: 'input',
+    name: 'Usage',
+    message: 'Descibe the usage of this project',
+},
+{
+    type: 'input',
+    name: 'Credits',
+    message: 'Are there any credits to note for this project?',
+},
+{
+    type: 'list',
+    name: 'License',
+    message: 'Select a license for your project:',
+    choices: [
+        'MIT License',
+        'Apache License 2.0',
+        'GNU General Public License (GPL)',
+        'ISC',
+        'Unlicense',
+        'Artistic license 2.0',
+    ]
+},
+{
+    type: 'input',
+    name: 'Credits',
+    message: 'Are there any credits to note for this project?',
+},
+{
+    type: 'input',
+    name: 'Contribution',
+    message: 'Enter the contribution guidelines for your project:',
+},
+{
+    type: 'input',
+    name: 'Features',
+    message: 'Are there any features youd like to include for this project?',
+},
+{
+    type: 'input',
+    name: 'Tests',
+    message: 'Are there any tests for this project?',
+}
+];
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(function(answers){
+        console.log(answers)
+    })
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
 // Function call to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    // Map the license value to the corresponding badge URL or Markdown
+    const licenseBadges = {
+      'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+      'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+      // Add more license badges as needed
+    };
 
+    // Check if the license has a corresponding badge
+    if (licenseBadges.hasOwnProperty(license)) {
+      return licenseBadges[license];
+    } else {
+      // Return a default message if the license is not found in the badges list
+      return 'License: ' + license;
+    }
+  } else {
+    return '';
+  }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
